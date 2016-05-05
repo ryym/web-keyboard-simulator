e7d4b513bfa1c24fa588783bf1301758dcc9fb64
ryym <ryym.64@gmail.com>
2016-05-05 23:20:20 +0900

    chore: Remove prototype HTML and CSS

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { makeInputConverter } from './services/input-converter';
 import { makeOutputGenerator } from './services/output-generator';
 import { renderViewInto } from './views';
 
-require('./tmp.css');
-
 renderViewInto('main', {
   makeInputConverter,
   makeOutputGenerator
