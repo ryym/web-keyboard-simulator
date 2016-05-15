8656308b799ab02e01eb8f053de6716432852635
ryym <ryym.64@gmail.com>
2016-05-15 23:02:40 +0900

    feat: Add link to GitHub in Header

--- src/views/Header/Header.react.js
+++ src/views/Header/Header.react.js
@@ -7,6 +7,13 @@ export default function Header() {
         Keyboard Simulator
       </div>
       <nav className="header_nav">
+        <ul className="header_nav-list">
+          <li className="header_nav-item link-github">
+            <a href="https://github.com/ryym/web-keyboard-simulator">
+              GitHub
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
