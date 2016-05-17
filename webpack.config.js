e78d27741debd487a881756562265c71e0b7ce03
ryym <ryym.64@gmail.com>
2016-05-17 23:14:00 +0900

    chore(webpack): Improve build for production
    * Minify JS code
    * Run React in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,72 +4,91 @@ const HtmlTemplatePlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const pcssAutoprefixer = require('autoprefixer');
 
-const ENV = process.env.WEBPACK_ENV || 'development';
-
 const SRC_PATH = path.join(__dirname, 'src');
 const BUILD_PATH = path.join(__dirname, 'build');
 const HTML_TEMPLATE_PATH = path.join(SRC_PATH, 'index.template.html');
 
-const isDevEnv = (ENV === 'development');
+module.exports = defineWebpackConfig(process.env.NODE_ENV || 'development');
 
-module.exports = {
-  resolve: {
-    extensions: ['', '.js', '.react.js'],
-    alias: {
-      $services: path.join(SRC_PATH, 'services')
-    }
-  },
-
-  entry: SRC_PATH,
-
-  output: {
-    path: BUILD_PATH,
-    filename: 'bundle.js'
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [
-          'babel'
-        ],
-        include: SRC_PATH
-      },
-      {
-        test: /\.scss$/,
-        loaders: [
-          'style',
-          'css',
-          'postcss',
-          'sass'
-        ],
-        include: SRC_PATH
+function defineWebpackConfig(ENV) {
+  return {
+    resolve: {
+      extensions: ['', '.js', '.react.js'],
+      alias: {
+        $services: path.join(SRC_PATH, 'services')
       }
-    ]
-  },
+    },
 
-  postcss: () => [
-    pcssAutoprefixer({
-      browsers: 'last 2 versions'
-    })
-  ],
+    entry: SRC_PATH,
 
-  devServer: {
-    contentBase: BUILD_PATH,
-    iframe: true
-  },
+    output: {
+      path: BUILD_PATH,
+      filename: 'bundle.js'
+    },
 
-  plugins: [
-    new CleanWebpackPlugin(['build']),
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loaders: [
+            'babel'
+          ],
+          include: SRC_PATH
+        },
+        {
+          test: /\.scss$/,
+          loaders: [
+            'style',
+            'css',
+            'postcss',
+            'sass'
+          ],
+          include: SRC_PATH
+        }
+      ]
+    },
 
-    new HtmlTemplatePlugin({
-      template: HTML_TEMPLATE_PATH,
-      inject: 'body'
-    }),
+    postcss: () => [
+      pcssAutoprefixer({
+        browsers: 'last 2 versions'
+      })
+    ],
 
-    new webpack.DefinePlugin({
-      '$$Config.LOG_LEVEL': isDevEnv ? "'debug'" : "'info'"
-    })
-  ]
-};
+    devServer: {
+      contentBase: BUILD_PATH,
+      iframe: true
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(['build']),
+
+      new HtmlTemplatePlugin({
+        template: HTML_TEMPLATE_PATH,
+        inject: 'body'
+      })
+    ].concat(getPluginsFor(ENV))
+  };
+}
+
+function getPluginsFor(ENV) {
+  if (ENV === 'production') {
+    return [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': "'production'",
+        '$$Config.LOG_LEVEL': "'info'"
+      }),
+
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false }
+      })
+    ];
+  }
+
+  if (ENV === 'development') {
+    return [
+      new webpack.DefinePlugin({
+        '$$Config.LOG_LEVEL': "'debug'"
+      })
+    ];
+  }
+}
